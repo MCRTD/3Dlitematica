c4e283071e86ba13e1eb8d13f8f3be100f6a7550
phillychi3 <phillychi3@gmail.com>
2023-01-28 16:19:30 +0800

    0

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from t3dlitematica.litematicadecoder import Resolve
 import json
-data = Resolve(r"C:\Users\phill\Desktop\codes\3Dlitematica\20gt.litematic")
+data = Resolve(r"C:\Users\phill\Desktop\codes\3Dlitematica\8020sc.litematic")
 with open("./test.json", "w",encoding="utf8") as f:
     json.dump(data, f, indent=4)
-
-data = eval(data)
 print(data)
