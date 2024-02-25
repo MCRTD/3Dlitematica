230da89e23556628dad32559029bb9e0dab59533
phillychi3 <phillychi3@gmail.com>
2024-02-26 00:40:18 +0800

    適配multipart

--- t3dlitematica/objbuilder/mctoobj.py
+++ t3dlitematica/objbuilder/mctoobj.py
@@ -58,19 +58,31 @@ class Enity:
         self.y = y
         self.z = z
         self.center = [sum([x, x + 0.1]) / 2, sum([y, y + 0.1]) / 2, sum([z, z + 0.1]) / 2]
-        self.parents = {}
         self.thisdata = None
         self.textures = {}
-        self.element = []
-        self.objdata = {"blockname": self.name, "v": [], "vt": [], "f": [], "textures": []}
+        self.enitys = []
         self.rotatemode = None
         self.rotate = 0
-        self.start()
+        self.element = None
+        self.objdata = {"blockname": self.name, "v": [], "vt": [], "f": [], "textures": []}
+        self.parse()
+        self.merge()
 
-    def __getstate__(self):
-        return "custom debug info: exponent is " + self.parents
+    def merge(self):
+        # f要替換為全部v轉換的index
+        for i in self.enitys:
+            self.objdata["v"].extend(i.objdata["v"])
 
-    def start(self):
+        for i in self.enitys:
+            for j in i.objdata["f"]:
+                temp = []
+                for x in range(len(j)):
+                    temp.append(self.objdata["v"].index(i.objdata["v"][j[x] - 1]) + 1)
+                self.objdata["f"].append(temp)
+            self.objdata["vt"].extend(i.objdata["vt"])
+            self.objdata["textures"].extend(i.objdata["textures"])
+
+    def parse(self):
         with open(
             r"C:\Users\phill\OneDrive\Documents\coed_thing\3Dlitematica\temp\output.json",
             "r",
@@ -105,14 +117,71 @@ class Enity:
                 if "when" in i:
                     if "OR" in i["when"]:
                         for j in i["when"]["OR"]:
-                            if self.blockdata["Properties"][list(j.keys())[0]] == list(j.values())[0]:
+                            if (
+                                self.blockdata["Properties"][list(j.keys())[0]]
+                                == list(j.values())[0]
+                            ):
+                                if "y" in i["apply"]:
+                                    self.rotatemode = "y"
+                                    self.rotate = i["apply"]["y"]
+                                elif "x" in i["apply"]:
+                                    self.rotatemode = "x"
+                                    self.rotate = i["apply"]["x"]
                                 self.load_model(i["apply"]["model"].split("/")[-1])
                                 break
                     else:
-                        if self.blockdata["Properties"][list(i["when"].keys())[0]] == list(i["when"].values())[0]:
+                        if (
+                            self.blockdata["Properties"][list(i["when"].keys())[0]]
+                            == list(i["when"].values())[0]
+                        ):
+                            if "y" in i["apply"]:
+                                self.rotatemode = "y"
+                                self.rotate = i["apply"]["y"]
+                            elif "x" in i["apply"]:
+                                self.rotatemode = "x"
+                                self.rotate = i["apply"]["x"]
                             self.load_model(i["apply"]["model"].split("/")[-1])
                 else:
                     self.load_model(i["apply"]["model"].split("/")[-1])
+
+    def load_model(self, modelname, isparent=False):
+        with open(
+            r"C:\Users\phill\OneDrive\Documents\coed_thing\3Dlitematica\temp\output.json",
+            "r",
+            encoding="utf8",
+        ) as f:
+            model = json.load(f)["models"][modelname]
+            if "parent" in model:
+                self.load_model(model["parent"].split("/")[-1], True)
+            # if isparent:
+            #     self.parents[modelname] = model
+            if "textures" in model:
+                self.textures.update(model["textures"])
+            if "elements" in model:
+                self.element = model["elements"]
+        if not isparent:
+            self.enitys.append(Build_enity(self, self.element))
+
+
+class Build_enity:
+    def __init__(self, mother: Enity, elements):
+        self.x = mother.x
+        self.y = mother.y
+        self.z = mother.z
+        self.center = [
+            sum([self.x, self.x + 0.1]) / 2,
+            sum([self.y, self.y + 0.1]) / 2,
+            sum([self.z, self.z + 0.1]) / 2,
+        ]
+        self.parents = {}
+        self.textures = mother.textures
+        self.element = elements
+        self.objdata = {"v": [], "vt": [], "f": [], "textures": []}
+        self.rotatemode = mother.rotatemode
+        self.rotate = mother.rotate
+        self.start()
+
+    def start(self):
         for i in self.element:
             self.build_element(i)
         if self.rotatemode == "y":
@@ -122,25 +191,6 @@ class Enity:
             for i in range(len(self.objdata["v"])):
                 self.objdata["v"][i] = self.rotate_x(self.objdata["v"][i], self.rotate, self.center)
         print("-" * 50)
-        if self.objdata["blockname"] == "brewing_stand":
-            pprint(self.objdata)
-            pprint(self.textures)
-
-    def load_model(self, modelname,isparent=False):
-        with open(
-            r"C:\Users\phill\OneDrive\Documents\coed_thing\3Dlitematica\temp\output.json",
-            "r",
-            encoding="utf8",
-        ) as f:
-            model = json.load(f)["models"][modelname]
-            if "parent" in model:
-                self.load_model(model["parent"].split("/")[-1], True)
-            if isparent:
-                self.parents[modelname] = model
-            if "textures" in model:
-                self.textures.update(model["textures"])
-            if "elements" in model:
-                self.element.extend(model["elements"])
 
     def append_pos(self, thelist, item):
         """
@@ -282,8 +332,6 @@ class Enity:
                     self.add_texture(i)
                 self.add_vt(element["faces"][i])
 
-
-
     def rotate_y(self, point, angle, center):
         angle = math.radians(angle)
         x = point[0] - center[0]
@@ -335,4 +383,4 @@ class Enity:
 
 if __name__ == "__main__":
     # Enity(0, 0, 0, {"Name": "minecraft:redstone_block"}).start()
-    print(Enity.rotate_y(...,[10, 0, 2], 180, [5, 5, 5]))
+    print(Build_enity.rotate_y(..., [10, 0, 2], 180, [5, 5, 5]))
