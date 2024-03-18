26ae3870940f7de42cece73a45bed490bed4f3a4
phillychi3 <phillychi3@gmail.com>
2024-03-18 16:19:38 +0800

    feat: init cli

--- t3dlitematica/__main__.py
+++ t3dlitematica/__main__.py
@@ -1,12 +1,70 @@
-from click import command, echo, option
+import os
+import click
 from litematicadecoder import Resolve
 from objbuilder import objhandel
 from texturepackexport import convert_texturepack
 
 
-@command
-@option("--count", default=1, help="Number of greetings.")
-@option("--name", prompt="Your name", help="The person to greet.")
-def hello(count, name):
-    for _ in range(count):
-        echo(f"Hello, {name}!")
+class Litematica(click.ParamType):
+    name = "litematica"
+
+    def convert(self, value, param, ctx):
+        if not os.path.exists(value):
+            self.fail(f"{value} does not exist.")
+        if not value.endswith(".litematic"):
+            self.fail(f"{value} is not a litematica file.")
+        return value
+
+
+class LitematicaOrJson(click.ParamType):
+    name = "LitematicaOrJson"
+
+    def convert(self, value, param, ctx):
+        if not os.path.exists(value):
+            self.fail(f"{value} does not exist.")
+        if not value.endswith(".litematic") or not value.endswith(".json"):
+            self.fail(f"{value} is not a litematica or json file.")
+        return value
+
+
+@click.group()
+@click.option("--debug", default=False)
+def cli(debug):
+    if debug:
+        click.echo("Debug mode is 'on' ")
+
+
+@cli.command()
+@click.argument("litematica", type=Litematica())
+@click.option("-o", "--output", "output", default="./", help="Output file path")
+def Decode(litematica, output):
+    """
+    Decode a litematica file to json file
+    """
+    print(litematica)
+    click.echo("Wait")
+
+
+@cli.command()
+@click.argument("json/litematica", type=LitematicaOrJson())
+@click.argument("TextureFolder", type=click.Path(exists=True))
+@click.option("-o", "--output", "output", default="./", help="Output file path")
+def Obj(data,testure,output):
+    """
+    Convert a litematica file to obj file
+    """
+    click.echo("Wait")
+
+
+@cli.command()
+@click.argument("texturepack", type=click.Path(exists=True))
+@click.option("-o", "--output", "output", default="./", help="Output file path")
+def Texture(texturepack,output):
+    """
+    Convert texture pack for 3d litematica
+    """
+    click.echo("Wait")
+
+
+if __name__ == "__main__":
+    cli()
